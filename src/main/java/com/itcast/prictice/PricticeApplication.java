b834d737bdf9e492d4ffda46607e1b478b52f74c
Weiyanlin123 <17621257791@163.com>
2021-08-16 23:08:22 +0800

    2222

---
+++ src/main/java/com/itcast/prictice/PricticeApplication.java
@@ -0,0 +1,13 @@
+package com.itcast.prictice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricticeApplication.class, args);
+	}
+
+}
